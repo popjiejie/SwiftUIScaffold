acec519f112f0b6c122509213456db6dcd02c3ed
Drew Fry <99735821+popjiejie@users.noreply.github.com>
2023-10-10 02:31:34 -0500

    Add files via upload

---
+++ MyLibrary/MyLibrary/Sources/swiftuiscaffold/swiftuiscaffold.swift
@@ -0,0 +1,22 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+import SwiftUI
+
+struct 页面脚手架<背景: View, 标题: View>: View {
+    let 背景区域: 背景
+    let 标题区域: 标题
+    
+    init(背景色: () -> 背景, 标题: () -> 标题) {
+        self.背景区域 = 背景色()
+        self.标题区域 = 标题()
+    }
+    
+    var body: some View {
+        ZStack {
+            背景区域
+            标题区域
+        }
+    }
+}
